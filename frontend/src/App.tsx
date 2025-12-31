9338b169cf1752a3f97f06ea57930f79f141b88c
Jonathan Cai <137264568+jonathanycai@users.noreply.github.com>
2025-12-31 00:31:05 -0800

    add analytics (#52)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ProtectedRoute } from "./components/layout/ProtectedRoute";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Pages
 import Home from "./pages/Home";
@@ -30,6 +32,8 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
+        <Analytics />
+        <SpeedInsights />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Welcome />} />
